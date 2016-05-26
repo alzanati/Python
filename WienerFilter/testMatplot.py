b4a99ef07fbd6d24f5df230d135e35507659777d
alzanati <hosny1993.bio@gmail.com>
2016-05-26 02:08:46 +0200

    cleaning branch

--- WienerFilter/testMatplot.py
+++
@@ -1,9 +0,0 @@
-"""
-@author: Mohamed Hosny Ahmed
-@purpose: test matplot package
-"""
-
-import matplotlib.pyplot as plt
-import numpy as np
-import wienerfilter
-
--- face_recogintion/test_recognition.py
+++ face_recogintion/test_recognition.py
@@ -7,9 +7,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
 
+
+
+
 if __name__ == '__main__':
-    im = cv2.imread("/home/mohamed/Pictures/hwa_feh_eih.png")
-    cv2.namedWindow(winname='image',flags=cv2.WINDOW_AUTOSIZE)
-    cv2.imshow(winname='image', mat=im)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    x = np.array([[90, 60, 90],
+                  [90, 90, 30],
+                  [60, 60, 60],
+                  [60, 60, 90],
+                  [30, 30, 30]], np.float32)
+    print('buit-in\n', np.cov(x))
+    y = zero_mean_data(x)
+    z = covariance(y)
+    print(z)
