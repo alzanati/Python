0229b47c23c9e84359d245345894773ce4059812
alzanati <hosny1993.bio@gmail.com>
2016-05-27 22:09:01 +0200

    get eigin faces

--- face_recogintion/FaceRecognition.py
+++ face_recogintion/FaceRecognition.py
@@ -10,15 +10,17 @@ from scipy import signal as sg
 
 class FaceRecognition:
     def __init__(self, csvFile):
-        self.smileFeatureVector = []    # list to hold smild image vectors
-        self.sadFeatureVector = []      # list to hold sad image vectors
-        self.smilePaths = []            # smile faces paths
-        self.sadPaths = []              # sad faces paths
-        self.dataPaths = []             #
-        self.absPaths = []              # absolute paths without ";"
-        self.absPath = csvFile          # absolute path for data sets
-        self.seekLength = 0             # data set count
-        self.testImagesCount = 49       # to get 76 smile and 76 sad
+        self.smileFeatureVector = []  # list to hold smild image vectors
+        self.sadFeatureVector = []  # list to hold sad image vectors
+        self.smilePaths = []  # smile faces paths
+        self.sadPaths = []  # sad faces paths
+        self.dataPaths = []  #
+        self.absPaths = []  # absolute paths without ";"
+        self.normalizedSad = np.array(0)  # store zero mean data
+        self.normalizedSmile = np.array(0)  # store zero mean data
+        self.absPath = csvFile  # absolute path for data sets
+        self.seekLength = 0  # data set count
+        self.testImagesCount = 49  # to get 76 smile and 76 sad
 
         self.load_data_from_csv()
         self.run()
@@ -41,7 +43,7 @@ class FaceRecognition:
             elif tmpChar == 'a':
                 self.sadPaths.append(tmpStr)
 
-        #   read smile images and convert them to (m x n) x 1 vector, and add it to list
+        # read smile images and convert them to (m x n) x 1 vector, and add it to list
         for i in range(len(self.smilePaths)):
             img = cv2.imread(self.smilePaths[i])
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -49,8 +51,7 @@ class FaceRecognition:
             self.smileFeatureVector.append(img)
         self.smileFeatureVector = np.asarray(self.smileFeatureVector)
         self.smileFeatureVector = self.smileFeatureVector.T[0]
-        print('smiles: \n', self.smileFeatureVector.shape)
-
+        print('smiles feature vector: \n', self.smileFeatureVector.shape)
 
         #   read sad images and convert them to (m x n) x 1 vector, and add it to list
         for i in range(len(self.sadPaths)):
@@ -60,7 +61,7 @@ class FaceRecognition:
             self.sadFeatureVector.append(img)
         self.sadFeatureVector = np.asarray(self.sadFeatureVector)
         self.sadFeatureVector = self.sadFeatureVector.T[0]
-        print('sads: \n', self.sadFeatureVector.shape)
+        print('sads feature vector: \n', self.sadFeatureVector.shape)
 
         self.absPaths.clear()
         self.dataPaths.clear()
@@ -88,7 +89,6 @@ class FaceRecognition:
     def zero_mean_data(data):
         n = data.shape[1]
         zero_data = []
-        print(n)
         for i in range(n):
             tmp = data[:, i]
             zero_data.append(tmp - tmp.mean())
@@ -105,40 +105,47 @@ class FaceRecognition:
 
     def run(self):
         # calculate cov matrix
-        self.sadCovMatrix = self.zero_mean_data(self.sadFeatureVector)
-        self.smilCovMatrix = self.zero_mean_data(self.smileFeatureVector)
-        self.sadCovMatrix = self.covariance(self.sadCovMatrix)
-        self.smilCovMatrix = self.covariance(self.smilCovMatrix)
-        print(self.sadCovMatrix.shape)
-        print(self.smilCovMatrix.shape)
+        self.normalizedSad = self.zero_mean_data(self.sadFeatureVector)
+        self.normalizedSmile = self.zero_mean_data(self.smileFeatureVector)
+        print('smile shape: ', self.normalizedSmile.shape)
+        print('sad shape: ', self.normalizedSad.shape)
+
+        self.sadCovMatrix = self.covariance(self.normalizedSad)
+        self.smilCovMatrix = self.covariance(self.normalizedSmile)
+        print('sad cov matrix shape: ', self.sadCovMatrix.shape)
+        print('smile cov matrix shape: ', self.smilCovMatrix.shape)
 
         # now we have max 75 eiginvector for each category
         self.eigValsSad, self.eigVecsSad = np.linalg.eig(self.sadCovMatrix)
         self.weight = (self.eigValsSad / self.eigValsSad.max()) * 100
-        print(self.eigValsSad[self.weight > 1.3])
-        self.weight = self.weight > 1.3         # get boolean indexing to access vectors
-        print(self.weight)
+        # print(self.eigValsSad[self.weight > 1.3])
+        self.weight = self.weight > 1.3  # get boolean indexing to access vectors
         self.eigVecsSad = self.eigVecsSad[self.weight]
-        print(self.eigVecsSad)
+        print(self.eigVecsSad.shape)
 
         self.eigValsSmile, self.eigVecsSmile = np.linalg.eig(self.smilCovMatrix)
         self.weight = (self.eigValsSmile / self.eigValsSmile.max()) * 100
-        print(self.eigValsSmile[self.weight > 1.3])
-        self.weight = self.weight > 1.3         # get boolean indexing to access vectors
-        print(self.weight)
+        self.weight = self.weight > 1.3  # get boolean indexing to access vectors
         self.eigVecsSmile = self.eigVecsSmile[self.weight]
-        print(self.eigVecsSmile)
+        print(self.eigVecsSmile.shape)
 
-    # #   project data on vector to get weight matrix choose only 50 vector
-    #     adjusted_data = data.T * eig_vecs
-    #
-    # #   get new vector and compare with old vectors with ssd
-    #     new_vector = eig_vecs * new_data.T
-    #
-    # #   get normalized cross correlation
-    #     e = new_vector - adjusted_data
+        #   project data on vector to get weight matrix of sads and smiles to get eigin faces
+        self.sadWeightMatrix = np.dot(self.eigVecsSad, self.normalizedSad.T)
+        print('weight matrix of sads: ', self.sadWeightMatrix.shape)
 
-    #   get ROC curve
+        self.smileWeightMatrix = np.dot(self.eigVecsSmile, self.normalizedSmile.T)
+        print('weight matrix of smiles: ', self.smileWeightMatrix.shape)
+
+        #   get new vector and compare with old vectors with ssd
+        new_image = 0
+
+
+        #     new_vector = eig_vecs * new_data.T
+        #
+        # #   get normalized cross correlation
+        #     e = new_vector - adjusted_data
+
+        #   get ROC curve
 
 
 if __name__ == '__main__':
